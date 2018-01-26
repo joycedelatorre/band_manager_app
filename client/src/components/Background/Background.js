a335e082393eff280c6ab31e33e38eb334732938
joyce <joycedelatorre87@gmail.com>
2018-01-25 23:48:49 -0600

    created user login

---
+++ client/src/components/Background/Background.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import "./Background.css";
+
+// The ...props means, spread all of the passed props onto this element
+// That way we don't have to define them all individually
+const Background = props => 
+	<div className="body"></div>;
+
+export default Background;
+
+
+    

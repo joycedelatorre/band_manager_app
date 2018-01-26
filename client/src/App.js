aa609e0bf5bc2340bc8ce73f81b3c1792546b3b0
joyce <joycedelatorre87@gmail.com>
2018-01-25 23:57:54 -0600

    user login w/background image

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,20 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import axios from 'axios';
 import './App.css';
+import Login from './pages/Login';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title" >React</h1>
+      <Login />
+       {/*<header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title" >React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+         <p className="App-intro">
+           To get started, edit <code>src/App.js</code> and save to reload.
+         </p>*/}
       </div>
     );
   }

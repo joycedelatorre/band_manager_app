4197e2eff69efdf7f919e9f5136ca9f7e5ee64fb
MDOUGLAS83 <mrdouglas83@gmail.com>
2018-02-05 16:46:16 -0600

    update pre-heroku deployment

--- routes/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const bandRoutes = require("./bands");
-const userRoutes = require("./users");
-
-router.use("/bands", bandRoutes);
-router.use("/users", userRoutes);
-
-module.exports = router;
